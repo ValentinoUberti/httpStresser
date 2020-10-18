3497dba3e9d224b58b3542f2de8db8a9b35a7406
ValentinoUberti <vuberti@redhat.com>
2020-10-18 11:27:30 +0200

    added -r option

--- example.sh
+++ example.sh
@@ -1,1 +1,1 @@
-ab -c 1000 -n 2000000 -w demo-jvm.demo-jvm.svc.cluster.local:8080/ > "/var/www/html/$(date +'%m_%d_%Y_%T')_results.html"
+ab -r -c 1000 -n 2000000 -w demo-jvm.demo-jvm.svc.cluster.local:8080/ > "/var/www/html/$(date +'%m_%d_%Y_%T')_results.html"
