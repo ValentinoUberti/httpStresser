705065bbab785dffc6fb7f9466538fafbd8217b2
ValentinoUberti <vuberti@redhat.com>
2020-10-19 09:29:02 +0200

    Examples

--- example.sh
+++ example.sh
@@ -1,1 +1,4 @@
-ab -r -c 1000 -n 2000000 -w demo-jvm.demo-jvm.svc.cluster.local:8080/ > "/var/www/html/$(date +'%m_%d_%Y_%T')_results.html"
+#ab -s 99 -r -c 100 -n 10000 -w demo-jvm.demo-jvm.svc.cluster.local:8080/ > "/var/www/html/$(date +'%m_%d_%Y_%T')_results.html"
+#ab -s 99 -r -c 100 -n 10000 -w quarkus-jvm.demo-quarkus.svc.cluster.local:8080/ > "/var/www/html/$(date +'%m_%d_%Y_%T')_results.html"
+#ab -s 99 -r -c 100 -n 10000 -w quarkus-native.demo-quarkus-native.svc.cluster.local:8080/ > "/var/www/html/$(date +'%m_%d_%Y_%T')_results.html"
+
