f963de9c41335742cd2a20f71aece11530d47579
ValentinoUberti <vuberti@redhat.com>
2020-10-21 10:37:35 +0200

    Istio

--- example.sh
+++ example.sh
@@ -5,7 +5,7 @@
 #ab -s 99 -r -c 100 -n 10000 -w quarkus-native.demo-quarkus-native.svc.cluster.local:8080/demo > "/var/www/html/$(date +'%m_%d_%Y_%T')_results.html"
 
 ### Istio
-#seq 1 1000 | xargs -n1 -P10 curl istio-ingressgateway-istio-system.apps.ocp.example.net/jvm
-#seq 1 1000 | xargs -n1 -P10 curl istio-ingressgateway-istio-system.apps.ocp.example.net/quarkus-jvm-istio
-#seq 1 1000 | xargs -n1 -P10 curl istio-ingressgateway-istio-system.apps.ocp.example.net/quarkus-native-istio
+#seq 1 100000 | xargs -n1 -P20 curl istio-ingressgateway-istio-system.apps.ocp.example.net/jvm
+#seq 1 100000 | xargs -n1 -P20 curl istio-ingressgateway-istio-system.apps.ocp.example.net/quarkus-jvm-istio
+#seq 1 100000 | xargs -n1 -P20 curl istio-ingressgateway-istio-system.apps.ocp.example.net/quarkus-native-istio
 
